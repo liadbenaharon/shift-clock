2913deb29b090096120bdb8d48bc71bae4c9055c
liadbenaharon <liadbenaharon@gmail.com>
2026-08-21 11:17:28 +0300

    Fix Android package ID to match Firebase

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.liadbenaharon.shiftclock',
+  appId: 'com.liad.shiftclock',
   appName: 'שעון נוכחות',
   webDir: 'web',
   server: {
